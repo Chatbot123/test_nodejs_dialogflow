82d540b7465b2dc875ba5d4b4a3de7eb96dbeac6
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2019-05-30 12:29:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
 
 
-app.post('/getmeasure-state', (req, res) => 
+app.post('/', (req, res) => 
 {
 		  console.log(req.body);
 		  
